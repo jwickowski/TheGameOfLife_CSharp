f04c941591fe3c459f0612bcfb0a033890bba5ae
Jerzy Wickowski <jwickowski@objectivity.co.uk>
2016-04-22 16:53:37 +0200

    refactor LifeGame test to not duplicate the some cose

--- TheGameOfLife/TheGameOfLife.Core.Tests/LifeGameTest.cs
+++ TheGameOfLife/TheGameOfLife.Core.Tests/LifeGameTest.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using tgol.App;
 using Xunit;
+using Xunit.Sdk;
 
 namespace TheGameOfLife.Core.Tests
 {
@@ -9,77 +10,90 @@ namespace TheGameOfLife.Core.Tests
         [Fact]
         public void oneCell()
         {
-            var board = new BoardArrayImp(new bool[1, 1] { { true } });
-            var lifeGame = new LifeGame(board);
-            Board newBoard = lifeGame.NextRound();
+            var inputArray = new bool[1, 1] {{true}};
+            var expectedArray = new [,] {{false}};
 
-            newBoard.IsAlive(0, 0).Should().BeFalse();
+            ExecuteTest(inputArray, expectedArray);
         }
 
         [Fact]
         public void twoCells()
         {
+            var inputArray = new bool[1, 2] {{true, true}};
+            var expectedArray = new bool[,] {{false, false}};
 
-            var board = new BoardArrayImp(new bool[1, 2] { { true, true } });
-            var lifeGame = new LifeGame(board);
-            Board newBoard = lifeGame.NextRound();
-
-            new ArrayableBoardDecorator(newBoard).GetArray().ShouldBeEquivalentTo(new bool[,] { { false, false } });
+            ExecuteTest(inputArray, expectedArray);
         }
 
         [Fact]
         public void threeInRow_after_one_round()
         {
-            var board = new BoardArrayImp(new bool[3, 3]
+            var inputArray = new bool[3, 3]
             {
-                { false, false, false },
-                { true, true, true },
-                { false, false, false }
-            });
-            var lifeGame = new LifeGame(board);
-            Board newBoard = lifeGame.NextRound();
+                {false, false, false},
+                {true, true, true},
+                {false, false, false}
+            };
+            var expectedArray = new bool[,]
+            {
+                {false, true, false},
+                {false, true, false},
+                {false, true, false}
+            };
 
-            new ArrayableBoardDecorator(newBoard).GetArray().ShouldBeEquivalentTo(new bool[,] { 
-                { false, true, false },
-                { false, true, false },
-                { false, true, false } });
+            ExecuteTest(inputArray, expectedArray);
         }
 
         [Fact]
         public void Fact5()
         {
-            var board = new BoardArrayImp(new bool[3, 3]
+            var inputArray = new bool[3, 3]
             {
-                { false, true, false },
-                { true, true, true },
-                { false, false, true }
-            });
-            var lifeGame = new LifeGame(board);
-            Board newBoard = lifeGame.NextRound();
+                {false, true, false},
+                {true, true, true},
+                {false, false, true}
+            };
 
-            new ArrayableBoardDecorator(newBoard).GetArray().ShouldBeEquivalentTo(new bool[,] {
-                { true, true, true },
-                { true, false, true },
-                { false, false, true } });
+            var expectedArray = new bool[,]
+            {
+                {true, true, true},
+                {true, false, true},
+                {false, false, true}
+            };
+
+            ExecuteTest(inputArray, expectedArray);
         }
 
         [Fact]
         public void threeInRow_after_two_round()
         {
-            var board = new BoardArrayImp(new bool[3, 3]
+
+            var inputArray = new bool[3, 3]
             {
                 { false, false, false },
                 { true, true, true },
                 { false, false, false }
-            });
-            var lifeGame = new LifeGame(board);
-            lifeGame.NextRound();
-            Board newBoard = lifeGame.NextRound();
+            };
+            var expectedArray = new bool[,]
+            {
+                {false, false, false},
+                {true, true, true},
+                {false, false, false}
+            };
 
-            new ArrayableBoardDecorator(newBoard).GetArray().ShouldBeEquivalentTo(new bool[,] {
-               { false, false, false },
-                { true, true, true },
-                { false, false, false }});
+            ExecuteTest(inputArray, expectedArray, rounds:2);
+        }
+
+        private void ExecuteTest(bool[,] inputArray, bool[,] expectedArray, int rounds = 1)
+        {
+            var board = new BoardArrayImp(inputArray);
+            var lifeGame = new LifeGame(board);
+            Board newBoard = null;
+            for (int i = 0; i < rounds; i++)
+            {
+                newBoard = lifeGame.NextRound();
+            }
+            new ArrayableBoardDecorator(newBoard).GetArray().ShouldBeEquivalentTo(expectedArray);
         }
     }
 }
